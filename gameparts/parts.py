8361fb85710bacbc5db937583e2ba21a5cfecee8
avdeevdmitrykrsk <153370353+avdeevdmitrykrsk@users.noreply.github.com>
2023-12-17 02:46:16 +0700

    Update parts.py, race.py

--- gameparts/parts.py
+++ gameparts/parts.py
@@ -1,4 +1,30 @@
+import pygame
+
+
+GRID_SIZE = 20
+
+
 class Car:
 
     def __init__(self):
         self.speed = 10
+        self.body_color = (255, 0, 0)
+        self.positions = [
+            (620, 600), (620, 620), (620, 640),
+            (600, 600), (600, 620), (600, 640),
+        ]
+
+    def draw(self, surface):
+        """Метод отрисовывает машину."""
+        for position in self.positions:
+            rect = (
+                pygame.Rect((position[0], position[1]), (GRID_SIZE, GRID_SIZE))
+            )
+            pygame.draw.rect(surface, self.body_color, rect)
+            pygame.draw.rect(surface, (93, 216, 228), rect, 1)
+        
+        head_rect = pygame.Rect(
+            (position[0], position[1]), (GRID_SIZE, GRID_SIZE)
+        )
+        pygame.draw.rect(surface, self.body_color, head_rect)
+        pygame.draw.rect(surface, (0, 0, 0), head_rect, 4)
--- race.py
+++ race.py
@@ -1,9 +1,45 @@
+import pygame
 from gameparts import Car
+
+pygame.init()
+
+# Константы для размеров
+GRID_SIZE = 20
+SCREEN_WIDTH, SCREEN_HEIGHT = 1240, 800
+GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
+GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
+
+# Направления движения
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+# Цвета фона
+BOARD_BACKGROUND_COLOR = (106, 250, 151)
+
+# Скорость движения
+SPEED = 20
+
+# Настройка игрового окна
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
+screen.fill(BOARD_BACKGROUND_COLOR)
+
+# Заголовок окна игрового поля
+pygame.display.set_caption('Гонка')
+
+# Настройка времени
+clock = pygame.time.Clock()
 
 
 def main():
+
     car1 = Car()
-    print(car1.speed)
+
+    while True:
+        clock.tick(SPEED)
+        car1.draw(screen)
+        pygame.display.update()
 
 
 if __name__ == '__main__':
